554e3ef5a65f8c809f79f3f5a5c1339c698b5fe6
Dhriti-Reddy <155660852+Dhriti-Reddy@users.noreply.github.com>
2024-01-06 09:59:39 +0530

    Add files via upload

---
+++ mains.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+      //  sticky navbar on scroll script  //
+      if (this.scrollY > 20) {
+        $(".navbar").addClass("sticky");
+      } else {
+        $(".navbar").removeClass("sticky");
+      }
+    });
+  
+    $(".navbar .menu li a").click(function () {
+      //  Smooth scroll on Menu Items click  //
+  
+      $("html").css("scrollBehavior", "smooth");
+    });
+  
+    //  Toggle Navbar  //
+  
+    $(".menu-btn").click(function () {
+      $(".navbar .menu").toggleClass("active");
+      $(".menu-btn i").toggleClass("active");
+    });
+})
+  
+    
